20b790f4274acffc2b662ebdbcc7f71d4fae3ea2
Alberto Mecca <alberto.mecca@cern.ch>
2025-07-23 15:35:24 +0200

    [cleanup] run_makeZX move constants definition and some logic outside main
    in an anonymous namespace, to understand where the numbers come from.
    
    * per-final-state scale factors and c_constant
    * file names, now in a std::map; the spline object name
    * use ROOT's Form() to format the name of the output file
    * the year is a string, to support things like 2022preEE

--- 4l_channel/data_driven_MC/src/run_makeZX.cpp
+++ 4l_channel/data_driven_MC/src/run_makeZX.cpp
@@ -10,11 +10,49 @@
 
 using namespace std;
 
+// Anonymous namespace to hold module-wide constants
+namespace {
+  static const vector<float> _fs_ROS_SS = {
+    1.04, //4mu
+    1.01, //4e
+    1.04, //2e2mu
+    1.00  //2mu2e
+  };
+  static const float c_constant = 14.0;
+
+  static const std::map<std::string, const char*> fileFRmap_ {
+    {"2016"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2016.root"},
+    {"2017"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2017.root"},
+    {"2018"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2018.root"},
+    {"2022preEE" , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIII_byZ1Z2/FRfiles/FakeRates_SS_2022.root"},
+    {"2022postEE", "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIII_byZ1Z2/FRfiles/FakeRates_SS_2022EE.root"},
+    {"2022preBPix", nullptr},
+    {"2023postBPix", nullptr},
+    {"2024", nullptr},
+    {"_end", nullptr}
+  };
+
+  static const std::map<std::string, const char*> fileDataMap_ {
+    {"2016"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2016/AllData/ZZ4lAnalysis.root"},
+    {"2017"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2017/AllData/ZZ4lAnalysis.root"},
+    {"2018"      , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2018/AllData/ZZ4lAnalysis.root"},
+    {"2022preEE" , "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIII_byZ1Z2/240820/2022/Data/AllData_2022.root"},
+    {"2022postEE", "/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIII_byZ1Z2/240820/2022EE/Data/AllData_2022EE.root"},
+    {"2022preBPix", nullptr},
+    {"2023postBPix", nullptr},
+    {"2024", nullptr},
+    {"_end", nullptr}
+  };
+
+  const char fileKDconstants[] = "$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/cconstants/SmoothKDConstant_m4l_DjjVBF13TeV.root";
+  const char splineName[] = "sp_gr_varReco_Constant_Smooth";
+}
+
 int FindFinalStateZX(short Z1Flav, short Z2Flav);
 
 int main( int argc, char *argv[] ){
 
-	int year = atoi(argv[1]);
+	std::string year = argv[1];
 	int enriched = atoi(argv[2]);
 
 	string theExtra = "";
@@ -34,34 +72,16 @@ int main( int argc, char *argv[] ){
 	int njet4 = 0;
 	int njet5p = 0;
 
-	vector<float> _fs_ROS_SS;
-	_fs_ROS_SS.push_back(1.04);//4mu
-	_fs_ROS_SS.push_back(1.01);//4e
-	_fs_ROS_SS.push_back(1.04);//2e2mu
-	_fs_ROS_SS.push_back(1.00);//2mu2e
-	// 2016
 
-	char name[200];
-	if (year == 2016) sprintf(name,"/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2016.root");
-	if (year == 2017) sprintf(name,"/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2017.root");
-	if (year == 2018) sprintf(name,"/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/FRfiles/FakeRates_SS_2018.root");
-
-	FakeRates *FR = new FakeRates(name);
+	const char* fileFR = fileFRmap_.at(year);
+	FakeRates *FR = new FakeRates(fileFR);
 
 	TChain *t = new TChain("CRZLLTree/candTree");
-	//2016
-	if (year == 2016) t->Add("/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2016/AllData/ZZ4lAnalysis.root");
-	//2017
-	if (year == 2017) t->Add("/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2017/AllData/ZZ4lAnalysis.root");
-	//2018
-	if (year == 2018) t->Add("/eos/cms/store/group/phys_higgs/cmshzz4l/cjlst/RunIILegacy/200205_CutBased/Data_2018/AllData/ZZ4lAnalysis.root");
+	const char* fileData = fileDataMap_.at(year);
+	t->Add(fileData);
 
-	float c_constant = 14.0;
-	//if (year == 2017) c_constant = 2.3;
-	//if (year == 2018) c_constant = 2.3;
-
-	TFile* f_ = TFile::Open("$CMSSW_BASE/src/ZZAnalysis/AnalysisStep/data/cconstants/SmoothKDConstant_m4l_DjjVBF13TeV.root");
-	TSpline3* ts = (TSpline3*)(f_->Get("sp_gr_varReco_Constant_Smooth")->Clone());
+	TFile* f_ = TFile::Open(fileKDconstants);
+	TSpline3* ts = (TSpline3*)(f_->Get(splineName)->Clone());
 	f_->Close();
 
 	candTree data(t);
@@ -114,7 +134,7 @@ int main( int argc, char *argv[] ){
 	float ZZMassErrCorr_new;
 	short njet;
 
-	sprintf(name,"ZX%d_noCut%s.root",year,theExtra.c_str());
+	const char* name = Form("ZX%s_noCut%s.root", year.c_str(), theExtra.c_str());
 	TFile *f = new TFile(name,"recreate");
 	TTree *tnew =new TTree("candTree","");
 	tnew->Branch("dbkg_kin",&dbkg_kin,"dbkg_kin/F");
